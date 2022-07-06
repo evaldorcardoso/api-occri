53dd9effd26cebced87f393e6db1ae53d0a17ba5
Evaldo Cardoso <evaldo.cardoso@soluti.com.br>
2022-07-06 16:21:41 -0300

    feat: listen port from .env

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
