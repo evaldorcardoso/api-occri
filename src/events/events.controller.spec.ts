e2ca30fe7f46a44c5a82f4ec83b4e514477ee916
Evaldo Cardoso <evaldo.cardoso@soluti.com.br>
2022-07-06 20:46:34 -0300

    tests: fix tests

--- src/events/events.controller.spec.ts
+++ src/events/events.controller.spec.ts
@@ -1,17 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EventsController } from './events.controller';
+import { EventsRepository } from './events.repository';
 import { EventsService } from './events.service';
 
 describe('EventsController', () => {
   let controller: EventsController;
+  let repository: EventsRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [EventsController],
-      providers: [EventsService],
+      providers: [
+        EventsService,
+        {
+          provide: EventsRepository,
+          useValue: {
+            createEvent: jest.fn(),
+            findOne: jest.fn(),
+            findEvents: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<EventsController>(EventsController);
+    repository = module.get<EventsRepository>(EventsRepository);
   });
 
   it('should be defined', () => {
