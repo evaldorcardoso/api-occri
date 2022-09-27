a5e2b60e79b17f7f81744aeb2dd75381c5ab2e13
Evaldo Cardoso <evaldorcardoso@outlook.com>
2022-09-27 19:09:13 -0300

    fix: firebase private key

--- src/auth/firebase-auth.strategy.ts
+++ src/auth/firebase-auth.strategy.ts
@@ -26,7 +26,7 @@ export class FirebaseAuthStrategy extends PassportStrategy(
       credential: firebase.credential.cert({        
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
       }),
     });
   }
